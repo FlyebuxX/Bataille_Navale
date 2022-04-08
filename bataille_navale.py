642ec5c349b38d1b95ee97c0995151d2d4c739c5
CodeLyo-ko <adresseajeu@orange.Fr>
2022-04-08 11:47:00 +0200

    added files

--- bataille_navale.py
+++ bataille_navale.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+### Programme principal ###
 
-def creer_plateau():
-    pass
+fenetre = tk.Tk()
+fenetre.title("Bataille Navale")
+zone_dessin = tk.Canvas(width="1100", height="600", bg="white")
+zone_dessin.pack()
+fenetre.mainloop()
