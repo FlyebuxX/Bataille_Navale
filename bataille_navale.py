82ec09331465f9f07c288aecfd4f00b0e90bc86f
CodeLyo-ko <adresseajeu@orange.Fr>
2022-04-17 14:40:49 +0200

    supression fichier

--- bataille_navale.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-### Programme principal ###
-
-fenetre = tk.Tk()
-fenetre.title("Bataille Navale")
-zone_dessin = tk.Canvas(width="1100", height="600", bg="white")
-zone_dessin.pack()
-fenetre.mainloop()
